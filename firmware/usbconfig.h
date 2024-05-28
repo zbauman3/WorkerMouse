32f37402e122af4287ab0579ee3fb6bb21c593de
Zane Bauman <zanebauman55@gmail.com>
2024-05-28 10:13:25 -0400

    finish schema, add basic README

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -128,7 +128,7 @@ extern void usbEventResetReady(void);
  * This template uses obdev's shared VID/PID pair for HIDs: 0x16c0/0x5df.
  * Use this VID/PID pair ONLY if you understand the implications!
  */
-#define USB_CFG_DEVICE_ID 0x31, 0xe1
+#define USB_CFG_DEVICE_ID 0x80, 0xF4
 /* This is the ID of the product, low byte first. It is interpreted in the
  * scope of the vendor ID. If you have registered your own VID with usb.org
  * or if you have licensed a PID from somebody else, define it here. Otherwise
